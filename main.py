488ac53392f3f1360c84fa5dcdb8c4e23ec42716
TheOne5 <2433325-TheOne5@users.noreply.replit.com>
2023-04-10 14:57:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('OurFirst Streamlit App')
+st.subheader('Introducing Streamlit in Automate everything')
+st.write('Myfirst web app')
+#To run locally
+#streamlit run main.py
